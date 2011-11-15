d915fb8890725bf524417fd579e17c924e0e030b
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-11-15 00:12:47 -0500

    moved KNN Concepts

--- clusterlib/knn.py
+++ clusterlib/knn.py
@@ -5,8 +5,42 @@ import math
 from operator import itemgetter, attrgetter
 import random
 
-from concept import Concept
 from distance import euclidean as distance
+
+class Concept(object):
+    """
+    Simple wrapper for a concept that tracks its category assignments. 
+    This data structure is used for KNN clustering.
+    """
+
+    # initialize the wrapper
+    def __init__(self, value):
+        self.value = value
+        self._cluster = None
+        self._previous_cluster = None
+
+    # Properties to track cluster assignments
+    @property
+    def cluster(self):
+        return self._cluster
+        
+    @cluster.setter
+    def cluster(self, value):
+        """ Track previous cluster assignment auto-magically. """
+        self._previous_cluster = self._cluster
+        self._cluster = value
+    
+    @property
+    def previous_cluster(self):
+        return self._previous_cluster
+
+
+    # Python syntax sugar for string printing
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return repr(self.value)
 
 def kmeans(k, population, min_delta=0):
     """
